b6797cacbc3dacbba7a0fbf6c40606f9663ea45b
RaghuKV <raghuvenkatasamy@gmail.com>
2024-06-25 12:12:15 +0530

    commit

---
+++ MagudeesConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MagudeesConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
