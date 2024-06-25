89e0d3cbf8da4c3fb294592049b37b67a3f5f7c8
Sureshkumar <kumaresan.thandapany@gmail.com>
2024-06-25 10:55:58 +0530

    GIthub Excercise

--- Sample.ConsoleApp/Program.cs
+++ Sample.ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace Sample.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hai");
         }
     }
 }
