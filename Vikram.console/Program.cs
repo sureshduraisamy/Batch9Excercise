dd9a085ffc318eb8a4481ef0d9865731f8acaf65
vikramnagamanickam <nagamanickamvikram@mail.com>
2024-06-25 12:08:34 +0530

    console

--- Vikram.console/Program.cs
+++ Vikram.console/Program.cs
@@ -6,7 +6,7 @@ namespace Vikram.console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hii");
         }
     }
 }
