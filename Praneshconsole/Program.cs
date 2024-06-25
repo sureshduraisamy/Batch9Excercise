00e6a668cadba7f8c99d0ff8a952de0898d09606
pranesh-c <pranesh@chellamuthu@gmal.com>
2024-06-25 12:21:24 +0530

    name

--- Praneshconsole/Program.cs
+++ Praneshconsole/Program.cs
@@ -6,7 +6,7 @@ namespace Praneshconsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("pranesh");
         }
     }
 }
