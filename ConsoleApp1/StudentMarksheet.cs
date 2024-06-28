b1afcd9060ef3ef3c73c9768b443dc71abb3023b
kaviyarasan-a <kaviyarasanaruchamy@gmail.com>
2024-06-28 13:16:23 +0530

    tesrt

--- ConsoleApp1/StudentMarksheet.cs
+++ ConsoleApp1/StudentMarksheet.cs
@@ -12,9 +12,13 @@ namespace ConsoleApp1
             int tamil = 90;
             int maths = 70;
             int science = 97;
-            int total = english + tamil + maths + science;
-            Console.WriteLine(total);
-            Console.WriteLine(studentName);
+            decimal  total = english + tamil + maths + science;
+            Console.WriteLine("Total marks=" +total);
+            Console.WriteLine("Student name=" +studentName);
+            decimal percentage = (total / 400 * 100);
+            Console.WriteLine(percentage);
+
+
         }
             }
 }
