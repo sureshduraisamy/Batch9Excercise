4a2d5d2a2f31008ee81c0d64d4216e1f20d2b96c
kaviyarasan-a <kaviyarasanaruchamy@gmail.com>
2024-06-28 12:45:23 +0530

    test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class A
+    {
+
+        static void Main(string[] args)
+        {
+            new StudentMarksheet().calculateTotal();
+
+
+
+
+        }
+
+    }
+}
---
+++ ConsoleApp1/StudentMarksheet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class StudentMarksheet
+        {
+
+        public void calculateTotal()
+        {
+            string studentName = "Pranesh";
+            int english = 80;
+            int tamil = 90;
+            int maths = 70;
+            int science = 97;
+            int total = english + tamil + maths + science;
+            Console.WriteLine(total);
+            Console.WriteLine(studentName);
+        }
+            }
+}
