53f5ce5badad71405f5c69e6463274be5b7b0649
kaviyarasur <rkaviyarasu.ramasamy@gmail.com>
2024-06-28 13:04:17 +0530

    28.6.2024 task finished

--- kaviyarasu--r/Program.cs
+++ kaviyarasu--r/Program.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using Calculator;
+using studentMark;
 
 namespace kaviyarasu__r
 {
-    class Program
+    /*class Program
     {
         static void Main(string[] args)
         {
             Arithmetic Add = new Arithmetic();
                 Add.Addition();
         }
+    }*/
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            student_Mark total = new student_Mark();
+            total.Totalmark();
+
+        }
+
     }
 }
---
+++ kaviyarasu--r/student Mark.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentMark
+{
+    class student_Mark
+    {
+        public void Totalmark()
+        {
+            string Name = "kaviyarasu";
+            int maths = 90;
+            int science = 90;
+            int social = 85;
+            decimal subjecttotal = (maths + science + social);
+            Console.WriteLine("studentName=" + Name);
+            Console.WriteLine("totalMark="+subjecttotal);
+            decimal percentage = (subjecttotal / 300 * 100);
+            Console.WriteLine("percentage=" + percentage);
+        }
+
+
+       
+    }     
+}        
+   
