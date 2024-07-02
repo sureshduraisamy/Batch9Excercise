b74b24971af5cd093b67cdd910278d05b2a0627b
pranesh-c <pranesh@chellamuthu@gmal.com>
2024-07-02 13:02:19 +0530

    Exchange

---
+++ Praneshconsole/Exchange.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praneshconsole
+{
+    class Exchange
+    {
+        int a = 50;
+        int b = 90;
+        int c;
+
+        public void report()
+        {
+            int c = a;
+            a = b;
+            b = c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+   
+
+        
+
+
+
+    }
+}
--- Praneshconsole/Program.cs
+++ Praneshconsole/Program.cs
@@ -7,17 +7,31 @@ namespace Praneshconsole
     {
         static void Main(string[] args)
         {
+            Exchange obj = new();
+            obj.report();
+
+
+
+
+
+
+
+
+
+
+
+
            // new StudentRepoarts().CalculaTetotal();
 
 
 
 
-            variable obj = new  ();
-            obj.M2();
-            obj.m1();
-            obj.m3();
+           // variable obj = new  ();
+           // obj.M2();
+           // obj.m1();
+            //obj.m3();
 
-
+           
 
 
               // / Departement info = new Departement();
