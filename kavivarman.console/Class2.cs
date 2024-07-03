925eea378f50dca03e883c656f8e6accb8b3279c
Raja Mohamed <rajamohamed.mbsb@gmail.com>
2024-07-03 13:31:30 +0530

    first program

---
+++ kavivarman.console/Class2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kavivarman.console
+{
+    class Class2
+    {
+    
+        public void multiplication ()
+        {
+            Console.WriteLine(8 * 9);
+            Console.WriteLine(5 * 8);
+               
+            
+            
+      
+        }
+     
+    }
+}
---
+++ kavivarman.console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace kavivarman.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Hello World!");
+             //Class1 obj = new Class1();
+            //obj.addition();
+            Class2 obj = new Class2();
+                obj.multiplication();
+        }
+    }
+}
