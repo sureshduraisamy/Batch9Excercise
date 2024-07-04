919d1ccb3a97e8386624af928edf6e0fb0670d52
naveendevi <gnaveen.devi@gmail.com>
2024-07-04 11:38:21 +0530

    A

---
+++ naveendevi.consol/BiggestNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace naveendevi.consol
+{
+    class BiggestNumber
+    {
+        public void Biggestnum()
+        {
+            int firstvalue=Convert.ToInt32()
+
+
+
+        }
+
+           
+
+
+    }
+}
