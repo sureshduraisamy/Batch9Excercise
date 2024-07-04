60444ce85cff6a86ee75e4b4e6ddf5d56fd17690
vasanthvishnu <vasanthraj.selva@gmail.com>
2024-07-04 11:56:37 +0530

    change

---
+++ Vignesh/Percentage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Sample.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exchange obj = new
+            //
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
