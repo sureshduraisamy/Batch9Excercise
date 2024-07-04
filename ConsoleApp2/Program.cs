c59dc3115db6033fcfe1da1869e8400af7b4dbd9
vasanthvishnu <vasanthraj.selva@gmail.com>
2024-07-04 11:57:41 +0530

    Code chnages

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+
+
+        }
+    }
+}
---
+++ Vignesh/interest calculation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace LoanInterestcalculation
+{ 
+class interestcalculation
+    {
+        public int LoanAmount
+        public int loanTenture;
+        public int Interestrate;
+        public void interest()
+        {
+            Console.WriteLine("enter LoanAmount");
+            LoanAmount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter Loantenture");
+            loanTenture = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter Interestrate");
+            Interestrate = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+        public void Compoundinterest()
+        {
+            int userinterest = (LoanAmount * Interestrate * loanTenture |/ 100 ;
+            Console.WriteLine(userinterest);
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
