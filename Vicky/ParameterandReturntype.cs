894a07c3d775d5b414db7ef1d5194aca48afb0a7
vasanthvishnu <vasanthraj.selva@gmail.com>
2024-07-08 13:02:10 +0530

    ParameterandReturntype

---
+++ Vicky/ParameterandReturntype.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vicky
+{
+    class ParameterandReturntype
+    {
+        
+            public void method1()
+        {
+            int a = 25;
+            int b = 45;
+            Console.WriteLine(a + b);
+
+        }
+            public void method2(int age, int name, int gender)
+        {
+            Console.WriteLine();
+        }
+            public string method3()
+        {
+            return "vignesh";
+        }
+        
+            
+    }
+}
--- Vicky/Program.cs
+++ Vicky/Program.cs
@@ -6,14 +6,18 @@ namespace Vicky
     {
         static void Main(string[] args)
         {
-            studentgrade obj = new();
-            obj.totalmark();
 
-
-
-
-
-
+            ParameterandReturntype obj = new ParameterandReturntype();
+            obj.method1();
+            obj.method2(45, 55, 65);
+            Console.WriteLine(obj.method3());
+            
+            
+            
+            
+            
+            // studentgrade obj = new();
+           // obj.totalmark();
 
 
            // Bignumber obj = new();
