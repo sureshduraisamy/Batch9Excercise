c3913301b685b7981c0c968fbccf39a8b896a272
pranesh-c <pranesh@chellamuthu@gmal.com>
2024-07-10 09:38:51 +0530

    program

--- Praneshconsole/EvenorOdd.cs
+++ Praneshconsole/EvenorOdd.cs
@@ -10,45 +10,30 @@ namespace praneshconsole
     {
         public EvenorOdd() 
         {
-            for (int even=1;even<=50;even ++)
+            for (int even = 1; even <= 50; even++)
             {
-                if (even % 2 == 0) 
+                if (even % 2 == 0)
                 {
                     Console.WriteLine("even:" + even);
-                
-                }
 
+                }
+            }
                 for (int Odd = 50; Odd >= 1; Odd--) 
                 {
-                if (Odd % 2 != 0) 
-                    
-                        
+                    if (Odd % 2 != 0)
+                    {
+
+
                         Console.WriteLine("Odd:" + Odd);
-                    
-                                   
+
+                    }     
                 
                 }
-
-
-
-
 
             }
 
 
         }
-        
-        
-        
-        
-        
-       
-
-        
-
-
-
-
-
+    
     }
-}
+
