a0c72b9d62708a824bb3e3dde66d11be9dec9010
naveendevi <gnaveen.devi@gmail.com>
2024-07-11 15:14:02 +0530

    un finish

---
+++ naveendevi.consol/MarkList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace naveendevi.consol
+{
+    class MarkList
+    {
+        public MarkList()
+
+        {
+            int count = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("no. of students:" + count);
+            Student_mark[] students = new Student_mark[count];
+            Student_mark Sm = new Student_mark();
+            for (int i = 0; i < count; i++)
+            {
+               Sm. name = Console.ReadLine();
+                Sm.english
+                
+
+
+
+
+
+            }
+
+            
+
+
+
+            {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+        }
+    }
---
+++ naveendevi.consol/Student Mark .cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace naveendevi.consol
+{
+    class Student_Mark
+    {
+        public string name;
+        public int tamil  ;
+        public int english ;
+        public int maths ;
+        public int science ;
+        public int cs ;
+        public int eg;
+        
+        
+      public Student_Mark()
+        {
+
+            int count = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("No.of.Student:" + count);
+            string name = Console.ReadLine();
+            Console.WriteLine("Name+name");
+            int tamil = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Tamil;+tamil");
+            int english = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("English;+english");
+            int maths = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("maths;+maths");
+            int science = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Science;+science");
+            int cs = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("cs;+cs");
+            int Total = tamil + english + maths + science + cs;
+            Console.WriteLine("total+total");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
