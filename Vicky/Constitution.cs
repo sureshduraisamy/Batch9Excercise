79d56cb5bc77441eff80fdc57b7d98e409606bb6
vasanthvishnu <vasanthraj.selva@gmail.com>
2024-07-12 14:51:16 +0530

    canditate and constitution

---
+++ Vicky/Constitution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vicky
+{
+    class Constitution
+    {
+       public int count = 0;
+
+        public Canditate[] Enterdetails (Canditate D)
+        {
+            count = Convert.ToInt32(Console.ReadLine());
+            Canditate[] arr = new Canditate[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                Canditate c = new();
+                c.name = Console.ReadLine();
+                c.constitution = Console.ReadLine();
+                c.age = Convert.ToInt32(Console.ReadLine());
+                
+                arr[i] = c;
+
+            }
+
+           
+
+
+            return arr;
+        }
+    }
+}
--- Vicky/Program.cs
+++ Vicky/Program.cs
@@ -7,38 +7,60 @@ namespace Vicky
         static void Main(string[] args)
         {
 
-            Array obj = new Array();
+            Canditate obj = new Canditate();
+            obj.name = Console.ReadLine();
 
-            //EvenOrOdd obj = new EvenOrOdd();
-
-
-
-
-           //Vowel obj = new Vowel();
-            //obj.VowelFinder();
-
-
-
-
-
-
-
-          //ParameterandReturntype obj = new ParameterandReturntype();
-            //obj.method1();
-            //obj.method2(45, 55, 65);
-            //Console.WriteLine(obj.method3());
             
-            
-            
-            
-            
-            // studentgrade obj = new();
-           // obj.totalmark();
+            Constitution s = new Constitution();
+            Canditate[] c = s.Enterdetails(obj);
+
+       
+
+            for (int i = 0; i < s.count; i++)
+            {
+                Console.WriteLine(c[i].name);
+                Console.WriteLine(c[i].constitution);
+                Console.WriteLine(c[i].age);
+            }
 
 
-           // Bignumber obj = new();
-            //obj.large();
-            //obj.big();
+
+
+
+
+
+            //Array obj = new Array();
+
+                //EvenOrOdd obj = new EvenOrOdd();
+
+
+
+
+                //Vowel obj = new Vowel();
+                //obj.VowelFinder();
+
+
+
+
+
+
+
+                //ParameterandReturntype obj = new ParameterandReturntype();
+                //obj.method1();
+                //obj.method2(45, 55, 65);
+                //Console.WriteLine(obj.method3());
+
+
+
+
+
+                // studentgrade obj = new();
+                // obj.totalmark();
+
+
+                // Bignumber obj = new();
+                //obj.large();
+                //obj.big();
         }
     }
 }
