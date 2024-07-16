92fcd7898093b32deac48b577ffab782950aed65
Kumaresan <kumaresan.thandapany@gmail.com>
2024-07-16 12:28:37 +0530

    Property and Constructor

---
+++ Sample.ConsoleApp/PropertyandDestructorExtn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.ConsoleApp
+{
+    partial class PropertyandDestructor
+    {
+
+        public PropertyandDestructor(string s)
+        {
+
+        }
+    }
+}
