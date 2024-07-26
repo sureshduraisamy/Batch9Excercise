ec536149d83c0aec18cf673ca6b7fed20960c62d
vikramnagamanickam <nagamanickamvikram@mail.com>
2024-07-26 10:51:15 +0530

    WordCounting.cs

---
+++ VikramBusinessLayer/WordCount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VikramBusinessLayer
+{
+    public class WordCount
+    {
+        string A = "hi i am vikram";
+
+        public void source()
+        {
+            string[] result = A.Split(' ');
+            Console.WriteLine(result.Length);
+
+        }
+    }
+}
+public class countingword
+{
+    string A = "hi i am vikram";
+    int count;
+    public void countsword()
+    {
+        for(int i = 0; i < A.Length; i++)
+        {
+            if(A[i]==' ')
+            {
+                count++;
+            }
+        }
+        Console.WriteLine(count + 1);
+    }
+
+}
+
+
---
+++ VikramBusinessLayer/WordReverse.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VikramBusinessLayer
+{
+   public class WordReverse
+    {
+        string A = "hi i am vikram";
+        string b = " ";
+        public void wordcount()
+        {
+            string[] result = A.Split(' ');
+            for (int i = result.Length; i>0; i--)
+            {
+                b = b + result[i - 1] + " ";
+            }
+            Console.WriteLine(b);
+
+        }
+    }
+    public class ReverseSplit
+    {
+        string A = "hi  i am vikram";
+        string c;
+        string d;
+        string s;
+        public void words()
+        {
+            for(int i = A.Length - 1; i >= 0; i--)
+            {
+                c = c + A[i];
+                if(A[i] ==' ' || i == 0)
+                {
+                    for(int j = c.Length - 1; j >= 0; j--)
+                    {
+                        d = d + c[j] ;
+                    }
+                    c = "";
+                    s = d;
+
+
+                }
+                
+
+                
+
+                
+            }
+            Console.WriteLine(s);
+
+        }
+    }
+}
