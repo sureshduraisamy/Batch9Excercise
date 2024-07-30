0009ca346c479b8cbad3efde709eae662a031432
Kumaresan <kumaresan.thandapany@gmail.com>
2024-07-30 12:03:25 +0530

    Exception handling

---
+++ BusinessLayer/MemberResgirtation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public interface ICommunication
+    {
+
+        public void SendSMS();
+    }
+
+
+    public class MemberResgirtation : ExceptionHandling, ICommunication
+    {
+        public MemberResgirtation(int c,string j):base(c,j)
+        {
+           
+            Console.WriteLine($"MemberRegistration----{c}");
+
+        }
+
+        public override void M1()
+        {
+            //jhgjggjh
+        }
+
+        public void SendSMS()
+        {
+            //jasgfjdsg
+        }
+
+        public void RegisterUser()
+        {
+
+            try
+            {
+                int b = 0;
+                var a = 10 / b;
+                var name = Console.ReadLine();
+                //
+
+                base.SendEMail();
+                M3();
+
+            }catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+
+        protected override void SendEMail()
+        {
+            //Anaiyaan SMTP 
+        }
+        public void Order ()
+        {
+            SendEMail();
+        }
+    }
+}
--- Sample.ConsoleApp/Program.cs
+++ Sample.ConsoleApp/Program.cs
@@ -7,25 +7,38 @@ namespace Sample.ConsoleApp
         static void Main(string[] args)
         {
 
-            var obj = new BusinessLogic();
+            try
+            {
 
+                MemberResgirtation ob = new MemberResgirtation(8, "syre");
+                ob.RegisterUser();
 
+                //ParamandReturnType param = new ParamandReturnType();
+                //param.Addition();
+                //param.Addition(69, 80);
+                //string v = param.Addition(38,'m',"Suresh");
+                //Console.WriteLine(v);
 
-            ParamandReturnType param = new ParamandReturnType();
-            //param.Addition();
-            //param.Addition(69, 80);
-            //string v = param.Addition(38,'m',"Suresh");
-            Console.WriteLine(v);
+                //BiggestNumber big = new BiggestNumber();
+                //big.BiggestNumb();
 
+                //ReadData obj = new ReadData();
+                //obj.GetData();
+                //obj.PrintData();
+                //Arithmetic ovj = new Arithmetic();
+                //ovj.Addtiion();
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Check your arry size");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something Went wrong . PLease contact admin");
+            }
+            finally { 
 
-            //BiggestNumber big = new BiggestNumber();
-            //big.BiggestNumb();
-
-            //ReadData obj = new ReadData();
-            //obj.GetData();
-            //obj.PrintData();
-            //Arithmetic ovj = new Arithmetic();
-            //ovj.Addtiion();
+            }
 
         }
 
