4d66415a0780ef12bceed0ec0c7d4966b6f13bb2
GIRIDHARANDHANDAPANI <giridharandhandapani@gmail.com>
2024-08-06 13:35:05 +0530

    Creating SQL

---
+++ Giridharan.Console/SQL/GiriSQL.sql
@@ -0,0 +1,17 @@
+--06-08-2024
+Insert into Registration
+Values('Giri','Giri@123','giri@gmail.com',987654321,'adress')
+
+Insert into Registration(UserName,Password,Email,MobileNumber)
+values('hari','Hari@123','hari@gmail.com',984567236)
+
+update Registration
+SET 
+Password='Hai@123'
+where UserName='hari'
+
+Delete from Registration
+Where MobileNumber=987654321
+
+Select * from Registration
+
