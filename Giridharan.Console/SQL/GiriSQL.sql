f39c11a4c2102fc8e3d814f3895d805b4d0aafd0
GIRIDHARANDHANDAPANI <giridharandhandapani@gmail.com>
2024-08-07 12:35:40 +0530

    Using DDL inSQL and UsingKeywords

--- Giridharan.Console/SQL/GiriSQL.sql
+++ Giridharan.Console/SQL/GiriSQL.sql
@@ -15,3 +15,56 @@ Where MobileNumber=987654321
 
 Select * from Registration
 
+--07-08-2024
+create table Registration
+(
+RegistrationID bigint not null identity(1,1),
+Username nvarchar(100) not null ,
+Password nvarchar(20) not null,
+Email nvarchar(150) not null,
+MobileNumber bigint not null,
+Adress nvarchar(300) null
+)
+
+Drop table Registration
+
+Alter table Registration
+drop column Adress
+
+Alter table Registration
+add address nvarchar(300) not null default('address')
+
+alter table registration
+alter column username varchar(200) not null
+
+truncate table registration
+
+Select 
+	Username,Password 
+from 
+	Registration
+where 
+   Username ='Giri' and RegistrationID=1
+
+   Select 
+	UserName,Password 
+from 
+	Registration
+where 
+   UserName ='Hari' or RegistrationID=5
+
+   Select 
+	UserName,Password 
+from 
+	Registration
+where 
+   RegistrationId between 1 and 5
+
+   Select 
+	UserName,Password 
+from 
+	Registration
+Where username like '__r%'
+
+select * from registration
+order by username asc
