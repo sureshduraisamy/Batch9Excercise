85cfcb8c3df87395bf72be3d0d18e57069650b6d
GIRIDHARANDHANDAPANI <giridharandhandapani@gmail.com>
2024-08-16 13:05:15 +0530

    Creating SQL query for Join Methods

--- Giridharan.Console/SQL/GiriSQL.sql
+++ Giridharan.Console/SQL/GiriSQL.sql
@@ -5,15 +5,20 @@ Values('Giri','Giri@123','giri@gmail.com',987654321,'adress')
 Insert into Registration(UserName,Password,Email,MobileNumber)
 values('hari','Hari@123','hari@gmail.com',984567236)
 
+Insert into Registration(UserName,Password,Email,MobileNumber)
+values('mari','mari@123','mari@gmail.com',9878645378)
+
 update Registration
 SET 
 Password='Hai@123'
 where UserName='hari'
 
 Delete from Registration
-Where MobileNumber=987654321
+Where Username='jwgfiu'
 
 Select * from Registration
+drop table Registration
+truncate table registration
 
 --07-08-2024
 create table Registration
@@ -67,4 +72,24 @@ from
 Where username like '__r%'
 
 select * from registration
+order by username asc
+create table PersonDetails
+(
+PersonDetailsID bigint not null identity(1,1),
+PanNumber varchar(13) not null unique,
+AadhaarNumber bigint not null unique,
+RegistrationID bigint not null
+)
+select * from persondetails
+drop table PersonDetails
+
+
+Insert into persondetails (PanNumber,Aadhaarnumber,RegistrationId)
+values  ('HUJIY8798J',9867895876849,1)
+insert into persondetails (PanNumber,Aadhaarnumber,RegistrationId)
+values('JNYFJ5467J',9876789056,2)
+select* from Registration r inner join persondetails pd on r.registrationID = pd.registrationid
+select* from Registration r left outer join persondetails pd on r.registrationID = pd.registrationid
+select* from Registration r right outer join persondetails pd on r.registrationID = pd.registrationid
+select* from Registration r full outer join persondetails pd on r.registrationID = pd.registrationid
 order by username asc
