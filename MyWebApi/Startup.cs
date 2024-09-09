15e7e555bf328cff9953563615e48efd2e920819
vasanthvishnu <vasanthraj.selva@gmail.com>
2024-09-09 15:20:01 +0530

    Javascript post and get

--- MyWebApi/Startup.cs
+++ MyWebApi/Startup.cs
@@ -32,6 +32,7 @@ namespace MyWebApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyWebApi", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +44,14 @@ namespace MyWebApi
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyWebApi v1"));
             }
+            app.UseCors(builder =>
+            {
+                builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            });
+
 
             app.UseHttpsRedirection();
 
