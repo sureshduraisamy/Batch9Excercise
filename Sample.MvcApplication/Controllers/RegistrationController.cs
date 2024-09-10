1a28cf4c3ecd2c5469a9994d0f0efc3a2de9c8c4
SureshkumarDuraisamy <sureshkumar.duraisamy@gmail.com>
2024-09-10 11:43:01 +0530

    Code changes for mVC curd

---
+++ Sample.MvcApplication/Controllers/RegistrationController.cs
@@ -0,0 +1,98 @@
+﻿using DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.MvcApplication.Controllers
+{
+    public class RegistrationController : Controller
+    {
+
+        IRegistrationRepository reg = null;
+        public RegistrationController(IConfiguration configuration, IRegistrationRepository regis)
+        {
+            reg = regis;
+        }
+
+        // GET: RegistrationController
+        public ActionResult List()
+        {
+           var resutl =  reg.SelectALLUser();
+
+            return View("list", resutl);
+        }
+
+        // GET: RegistrationController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RegistrationController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RegistrationController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RegistrationController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RegistrationController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RegistrationController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RegistrationController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
